f4572c02582c8e37c79b0e3160ebf1dfb0dacde1
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-04-02 11:57:23 +0530

    Create binary-search.java

---
+++ Searching Algortihms/binary-search.java
@@ -0,0 +1,17 @@
+public static int binarySearch(int[] a,int value)
+{
+    int start = 0;
+    int end = a.length-1;
+    
+    while(start>=end)                                   //flow control condition
+    {
+        if(a[mid]==value)
+            return mid;
+        else if(value>a[mid])
+            start = mid+1;
+        else
+            end = mid-1;
+    }
+    
+    return -1;
+}
