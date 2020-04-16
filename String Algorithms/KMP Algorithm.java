223d9bff0eeace68b2ab336abbd72f5ee6074322
Astel Thottankara <57724542+astelThottankara@users.noreply.github.com>
2020-04-16 16:07:56 +0530

    Updated

--- String Algorithms/KMP Algorithm.java
+++ String Algorithms/KMP Algorithm.java
@@ -1,3 +1,61 @@
 /*
     String Searching Algorithm
 */
+
+public static void KMP_Algorithm(String pattern,String original)
+    {
+        int originalSize = original.length();
+        int patternSize = pattern.length();
+        int lps[] = new int[patternSize];
+        lpsArray(lps,patternSize,pattern);
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<originalSize)
+        {
+            if(original.charAt(i)==pattern.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            if(j==patternSize)
+            {
+                count++;
+                j = lps[j-1];
+            }
+            else if(i<patternSize && original.charAt(i)!=pattern.charAt(j))
+            {
+                if(j!=0)
+                    j = lps[j-1];
+                else    
+                    i++;
+            }
+        }
+        System.out.println(count);
+    }
+
+public static void generateLpsArray(int[] lps,int patternSize,String pattern)
+    {
+        int length = 0;
+        int i = 1;
+        lps[0] = 0;
+        while(i<patternSize)
+        {
+            if(pattern.charAt(i)==pat.charAt(len))
+            {
+                length++;
+                lps[i] = length;
+                i++;
+            }
+            else
+            {
+                if(length!=0)
+                    length = lps[length-1];
+                else
+                {
+                    lps[i] = length;
+                    i++;
+                }
+            }
+        }
+    }
